9b81cf9b4ef1775960b8a0feb89d22612d60dfe8
jovanovk <kevin.jovanovic@reutlingen-university.de>
2021-05-11 15:24:21 +0200

    init

---
+++ src/main/java/com/goblintech/boundry/CreationService.java
@@ -0,0 +1,15 @@
+package com.goblintech.boundry;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * CreationService
+ */
+@ApplicationPath("/api")
+public class CreationService extends Application {
+
+    
+}
